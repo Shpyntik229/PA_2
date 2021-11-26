b43bc23273bd7408ac1e1ee89168cbe1b30505f4
Shpyntik229 <dimaidanya18@gmail.com>
2021-11-26 10:28:51 +0200

    2

--- Laba2/main.cpp
+++ Laba2/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <fstream>
+#include <chrono>
 
 #include "RBTree.h"
 
@@ -21,7 +22,7 @@ void viewFirstElements(const RBTree<K, V>& tree, int count)
     }
 }
 
-int main()
+void test()
 {
     std::vector<int> vec;
     vec.reserve(10000);
@@ -40,11 +41,42 @@ int main()
     for (int i = 0; i < 15; i++)
     {
         int ind = std::rand() % 10000;
+        using namespace std::chrono;
+        std::chrono::time_point<std::chrono::steady_clock> curtime = std::chrono::high_resolution_clock::now();;
         if (tree[vec[ind]] != ind) throw 666;
+        std::size_t time = duration_cast<microseconds>(high_resolution_clock::now() - curtime).count();
         int checkCount = tree.getLastCheckCount();
-        out << i + 1 << "\t" << checkCount << "\n";
+        out << i + 1 << "\t" << checkCount << "\t" << time << "\n";
         acc += checkCount;
     }
     int mean = acc / 15;
     out << "\nMean: " << mean;
+}
+
+int main()
+{
+    test();
+    RBTree<int, int> tree;
+    std::cout << "Adding\n";
+    int key, value;
+    std::cin >> key >> value;
+    if (tree.add(key, value))
+    {
+        std::cout << "Key/Value succesfully added!\n";
+    }
+    else
+    {
+        std::cout << "ERROR!\n";
+    }
+    std::cout << "Finding\n";
+    std::cin >> key;
+    std::cout << "Value: ";
+    try
+    {
+        std::cout << tree[key] << std::endl;
+    }
+    catch (int err)
+    {
+        std::cout << "EMPTY" << std::endl;
+    }
 }
